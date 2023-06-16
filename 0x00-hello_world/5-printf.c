1127d1946f4548d02d5a8df1339a3a3213ed2d33
NiiLino <azumahhlin@gmail.com>
2023-06-15 18:41:43 -0700

    Commit update

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome 
- * is a piece of art,"
+ * main - prints "with proper grammar, but the outcome is a piece of
+ * art," folloed by a new line
  * Return: Always 0
  */
 int main(void)

a21ab04dab809209dfc9945c340f3bf32d966fe7
NiiLino <azumahhlin@gmail.com>
2023-06-16 12:48:46 -0700

    Commit update

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
-/** main - Entry point
+/*
+ * main - Entry point
  *
- * return: Always 0 (success)
+ * Return: 0 (success)
  */
 int main(void)
 {
@@ -12,7 +13,7 @@ int main(void)
 	c = 'a';
 	d = 'A';
 	while
-		(c <= 'z'){
+		(c <= 'z') {
 			putchar(c);
 			c++;
 		}

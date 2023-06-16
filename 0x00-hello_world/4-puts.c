0429f2239ee9b38c6ac0a9ec7d3b7a8558d2a07b
NiiLino <azumahhlin@gmail.com>
2023-06-15 17:44:33 -0700

    Commit update

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <studio.h>
+
+
+
+/**
+ * main - prints "Programming is like a building a multilingual puzzle,"
+ * Return: Always 0
+ */
+
+int main(void)
+{
+    puts("\"Programming is like building a multilingual puzzle")
+    return(0);
+}    

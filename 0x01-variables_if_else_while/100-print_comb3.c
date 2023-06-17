fbfa967008a4f4e6c07e08f1096504bd5163ce27
NiiLino <azumahhlin@gmail.com>
2023-06-16 20:38:29 -0700

    Commit update

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	int s = 0; e =1 , s1 = 1 , e1 = 2;
+	int s = 0, e = 8, s1 = 1, e2 = 9;
 	while ( s <= e)
 	{
 		s1 = s + 1;
@@ -14,12 +14,13 @@ int main(void)
 		{
 			putchar (s + '0');
 			putchar (s1 +'0');
+			s1 ++;
+
 			if (s != e)
 			{
 				putchar (',');
 				putchar(' ');
 			}
-			s1 ++;
 		}
 		s ++;
 	}

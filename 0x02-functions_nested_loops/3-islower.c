869f361f26c2f2cf7d6caaa70772c375ab7f9d3d
NiiLino <azumahhlin@gmail.com>
2023-06-20 14:48:34 -0700

    commit update

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - check is char is lowercase 
+ * _islower - check is char is lowercase
  * @c: is the char to be checked
  * Return: 1 if char is lowercase, otherwise 0.
  */
